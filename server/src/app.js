361b4b9b8d7a6443e9eb057b2f0903a5cb913b95
aimar.onebyone <aimar@onebyone.io>
2023-12-08 16:04:46 +0700

    fix path

--- server/src/app.js
+++ server/src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const userRoutes = require('./routes/users.routes');
+const userRoutes = require('./routes/userRoute');
 
 dotenv.config();
 
